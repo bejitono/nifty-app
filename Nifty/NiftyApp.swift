68669eb40404024f2915e96bed3da2589803695d
bejitono <stdemicheli@protonmail.com>
2021-08-12 13:55:56 +0700

    Sets up app.

---
+++ Nifty/NiftyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiftyApp.swift
+//  Nifty
+//
+//  Created by Stefano on 08.08.21.
+//
+
+import SwiftUI
+
+@main
+struct NiftyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NFTListView()
+        }
+    }
+}
