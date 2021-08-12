d14d0408561f4a456a37f12e8fc28556096bd018
bejitono <stdemicheli@protonmail.com>
2021-08-12 13:56:38 +0700

    Implements download task.

---
+++ Nifty/Networking/DownloadTask.swift
@@ -0,0 +1,118 @@
+//
+//  DownloadTask.swift
+//  Nifty
+//
+//  Created by Stefano on 12.08.21.
+//
+
+import Combine
+import Foundation
+
+typealias DownloadTaskResponse = (url: URL, urlResponse: URLResponse)
+
+extension URLSession {
+
+    public func downloadTaskPublisher(for url: URL) -> URLSession.DownloadTaskPublisher {
+        self.downloadTaskPublisher(for: .init(url: url))
+    }
+
+    public func downloadTaskPublisher(for request: URLRequest) -> URLSession.DownloadTaskPublisher {
+        .init(request: request, session: self)
+    }
+
+    public struct DownloadTaskPublisher: Publisher {
+
+        public typealias Output = (url: URL, response: URLResponse)
+        public typealias Failure = URLError
+
+        public let request: URLRequest
+        public let session: URLSession
+
+        public init(request: URLRequest, session: URLSession) {
+            self.request = request
+            self.session = session
+        }
+
+        public func receive<S>(subscriber: S) where S: Subscriber,
+            DownloadTaskPublisher.Failure == S.Failure,
+            DownloadTaskPublisher.Output == S.Input
+        {
+            let subscription = DownloadTaskSubscription(subscriber: subscriber, session: self.session, request: self.request)
+            subscriber.receive(subscription: subscription)
+        }
+    }
+}
+
+extension URLSession {
+
+    final class DownloadTaskSubscription<SubscriberType: Subscriber>: Subscription where
+        SubscriberType.Input == (url: URL, response: URLResponse),
+        SubscriberType.Failure == URLError
+    {
+        private var subscriber: SubscriberType?
+        private weak var session: URLSession!
+        private var request: URLRequest!
+        private var task: URLSessionDownloadTask!
+
+        init(subscriber: SubscriberType, session: URLSession, request: URLRequest) {
+            self.subscriber = subscriber
+            self.session = session
+            self.request = request
+        }
+
+        func request(_ demand: Subscribers.Demand) {
+            guard demand > 0 else {
+                return
+            }
+            self.task = self.session.downloadTask(with: request) { [weak self] url, response, error in
+                if let error = error as? URLError {
+                    self?.subscriber?.receive(completion: .failure(error))
+                    return
+                }
+                guard let response = response else {
+                    self?.subscriber?.receive(completion: .failure(URLError(.badServerResponse)))
+                    return
+                }
+                if let response = response as? HTTPURLResponse,
+                   !(200...299).contains(response.statusCode) {
+                    self?.subscriber?.receive(completion: .failure(URLError(.noPermissionsToReadFile)))
+                    return
+                }
+                guard let url = url else {
+                    self?.subscriber?.receive(completion: .failure(URLError(.badURL)))
+                    return
+                }
+                do {
+                    let cacheDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+                    
+                    print(response.mimeType)
+                    let fileUrl = cacheDir
+                        .appendingPathComponent(UUID().uuidString)
+                        .appendingPathExtension(response.mimeSubType) // or use response.url?.pathExtension?
+                    try FileManager.default.moveItem(atPath: url.path, toPath: fileUrl.path)
+                    _ = self?.subscriber?.receive((url: fileUrl, response: response))
+                    self?.subscriber?.receive(completion: .finished)
+                }
+                catch {
+                    self?.subscriber?.receive(completion: .failure(URLError(.cannotCreateFile)))
+                }
+            }
+            self.task.resume()
+        }
+
+        func cancel() {
+            self.task.cancel()
+        }
+    }
+}
+
+extension URLResponse {
+    
+    var mimeSubType: String {
+        String(self.mimeType?.split(separator: "/").last ?? "")
+    }
+    
+    var mimeMainType: String {
+        String(self.mimeType?.split(separator: "/").first ?? "")
+    }
+}
