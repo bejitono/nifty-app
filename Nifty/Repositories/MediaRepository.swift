bf598238e64356e6a5672332f53b94148fb8f714
bejitono <stdemicheli@protonmail.com>
2021-08-12 13:59:11 +0700

    Implements media repo.

---
+++ Nifty/Repositories/MediaRepository.swift
@@ -0,0 +1,40 @@
+//
+//  ImageRepository.swift
+//  Nifty
+//
+//  Created by Stefano on 08.08.21.
+//
+
+import Combine
+import Foundation
+
+protocol MediaFetcheable {
+    func fetchImageData(from url: URL) -> AnyPublisher<Media, Error>
+}
+
+final class MediaRepository: MediaFetcheable {
+    
+    private let networkClient: NetworkClient
+    
+    init(networkClient: NetworkClient = NetworkClientImpl()) {
+        self.networkClient = networkClient
+    }
+
+    // Do persistence
+    // check if file exitsts FileManager.default.fileExists(atPath: documentsDirectoryURL.appendingPathComponent(videoURL.lastPathComponent).path)
+    func fetchImageData(from url: URL) -> AnyPublisher<Media, Error> {
+        return networkClient.download(with: url)
+            .map(toMedia)
+            .mapError { $0 }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+    }
+    
+    private func toMedia(_ response: DownloadTaskResponse) -> Media {
+        Media(
+            url: response.url,
+            type: MediaType(from: response.urlResponse.mimeMainType),
+            fileType: FileType(from: response.urlResponse.mimeSubType)
+        )
+    }
+}
