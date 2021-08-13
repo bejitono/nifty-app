701bae9468d9dfd4f3f24c063801ebce758e1c52
bejitono <stdemicheli@protonmail.com>
2021-08-13 19:26:22 +0700

    Implements user cache.

--- Nifty/Persistence/UserCache.swift
+++ Nifty/Persistence/UserCache.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+protocol UserCacheable {
+    
+    func set<Data>(_ data: Data) where Data: UserCacheKeyConvertible
+    func get<Data>() -> Data? where Data: UserCacheKeyConvertible
+}
+
+protocol UserCacheKeyConvertible: Codable {
+    static var key: String { get }
+}
+
+final class UserCache: UserCacheable {
+    
+    private let userDefaults: UserDefaults
+    
+    init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+    }
+    
+    func set<Data>(_ data: Data) where Data: UserCacheKeyConvertible {
+        if let encoded = try? JSONEncoder().encode(data) {
+            userDefaults.set(encoded, forKey: Data.key)
+        }
+    }
+    
+    func get<Data>() -> Data? where Data: UserCacheKeyConvertible {
+        guard let data = userDefaults.object(forKey: Data.key) as? Foundation.Data,
+              let decoded = try? JSONDecoder().decode(Data.self, from: data) else {
+            return nil
+        }
+        return decoded
+    }
+}
