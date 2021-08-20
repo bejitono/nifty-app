a5b4521b052d0ded74b83c4e576ce92436f8c498
bejitono <stdemicheli@protonmail.com>
2021-08-20 09:28:29 +0700

    Adds image styles.

---
+++ Nifty/Views/Styles/Image.swift
@@ -0,0 +1,12 @@
+//
+//  Image.swift
+//  Nifty
+//
+//  Created by Stefano on 20.08.21.
+//
+
+import UIKit
+
+struct Images {
+    static let share = UIImage(systemName: "square.and.arrow.up")!
+}
