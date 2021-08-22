5815d4f0b7bf9e0869dc04ef66f1f9c2adac87a0
bejitono <stdemicheli@protonmail.com>
2021-08-22 20:22:49 +0700

    Implements textfield.

---
+++ Nifty/Views/AppTextField.swift
@@ -0,0 +1,37 @@
+//
+//  AppTextField.swift
+//  Nifty
+//
+//  Created by Stefano on 22.08.21.
+//
+
+import SwiftUI
+
+struct AppTextField: View {
+    
+    let placeholder: String
+    @Binding var text: String
+    
+    var body: some View {
+        TextField(placeholder, text: $text)
+            .frame(height: 30)
+            .padding(EdgeInsets(top: 10, leading: 15, bottom: 10, trailing: 15))
+            .overlay(
+                Capsule(style: .continuous)
+                    .stroke(
+                        Color.white,
+                        style: StrokeStyle(lineWidth: 3)
+                    )
+            )
+            .accentColor(.white)
+            .font(.headline)
+            .foregroundColor(.primary)
+    }
+}
+
+
+struct AppTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        AppTextField(placeholder: "address", text: .constant(""))
+    }
+}
