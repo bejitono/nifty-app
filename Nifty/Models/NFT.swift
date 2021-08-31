30c728d5e5d43b0ecf84f86020de49c79e39648a
bejitono <stdemicheli@protonmail.com>
2021-08-31 12:31:52 +0200

    Removes unecessary nft model properties.

--- Nifty/Models/NFT.swift
+++ Nifty/Models/NFT.swift
@@ -10,14 +10,11 @@ import Foundation
 
 struct NFT: Equatable {
     let timeStamp: String
-    let from: String
     let contractAddress: String
     let to: String
     let tokenID: String
     let tokenName: String
     let tokenSymbol: String
-    let tokenDecimal: String
-    let transactionIndex: String
     var metadata: ERC721Metadata?
     var media: Media?
 }
@@ -38,14 +35,11 @@ extension NFT {
         )
         let nft = NFT(
             timeStamp: self.timeStamp,
-            from: self.from,
             contractAddress: self.contractAddress,
             to: self.to,
             tokenID: self.tokenID,
             tokenName: self.tokenName,
             tokenSymbol: self.tokenSymbol,
-            tokenDecimal: self.tokenDecimal,
-            transactionIndex: self.transactionIndex,
             metadata: nil,
             media: nil
         )
@@ -59,41 +53,32 @@ extension NFT {
     
     init(_ dto: NFTDto) {
         self.timeStamp = dto.timeStamp
-        self.from = dto.from
         self.contractAddress = dto.contractAddress
         self.to = dto.to
         self.tokenID = dto.tokenID
         self.tokenName = dto.tokenName
         self.tokenSymbol = dto.tokenSymbol
-        self.tokenDecimal = dto.tokenDecimal
-        self.transactionIndex = dto.transactionIndex
     }
     
     init(_ dto: NFTCacheDto,
          _ mediaURL: String?) {
         self.timeStamp = dto.timeStamp
-        self.from = dto.from
         self.contractAddress = dto.contractAddress
         self.to = dto.to
         self.tokenID = dto.tokenID
         self.tokenName = dto.tokenName
         self.tokenSymbol = dto.tokenSymbol
-        self.tokenDecimal = dto.tokenDecimal
-        self.transactionIndex = dto.transactionIndex
         self.media = Media(dto.media, mediaURL)
         self.metadata = ERC721Metadata(dto.metadata)
     }
     
     init(_ dto: OpenSeaNFTDto) {
         self.timeStamp = ""
-        self.from = ""
         self.contractAddress = dto.contract.address
         self.to = ""
         self.tokenID = dto.tokenId
         self.tokenName = dto.contract.name
         self.tokenSymbol = dto.contract.symbol
-        self.tokenDecimal = ""
-        self.transactionIndex = ""
         self.metadata = ERC721Metadata(
             name: dto.name,
             image: dto.image ?? "",
@@ -125,14 +110,11 @@ struct EtherscanResponse<T: Codable>: Codable {
 
 struct NFTCacheDto: Codable {
     let timeStamp: String
-    let from: String
     let contractAddress: String
     let to: String
     let tokenID: String
     let tokenName: String
     let tokenSymbol: String
-    let tokenDecimal: String
-    let transactionIndex: String
     var metadata: ERC721MetadataCacheDto?
     var media: MediaCacheDto?
 }
@@ -141,14 +123,11 @@ extension NFTCacheDto {
     
     init(_ model: NFT) {
         self.timeStamp = model.timeStamp
-        self.from = model.from
         self.contractAddress = model.contractAddress
         self.to = model.to
         self.tokenID = model.tokenID
         self.tokenName = model.tokenName
         self.tokenSymbol = model.tokenSymbol
-        self.tokenDecimal = model.tokenDecimal
-        self.transactionIndex = model.transactionIndex
         self.media = MediaCacheDto(model.media)
         self.metadata = ERC721MetadataCacheDto(model.metadata)
     }
