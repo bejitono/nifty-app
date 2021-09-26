07fec7038382d48945935c5d61ce4a1488f836d6
bejitono <stdemicheli@protonmail.com>
2021-09-26 21:42:29 +0200

    Refactors nft list to use navigation view title.

--- Nifty/Views/NFTListView.swift
+++ Nifty/Views/NFTListView.swift
@@ -17,82 +17,79 @@ struct NFTListView: View {
     }
     
     var body: some View {
-        ZStack {
-            AppGradient()
-            VStack {
-                HStack {
-                    Title("My NFTs")
-                    Spacer()
+        NavigationView {
+            ZStack {
+                AppGradient()
+                VStack {
+                    ScrollView {
+                        LazyVStack(spacing: 40) {
+                            // TODO: view for empty and error state
+                            ForEach(viewModel.nftsViewModel, id: \.id) { nft in
+                                NFTView(nft: nft)
+                                    .equatable()
+                                    .cardStyle()
+                                    .onAppear {
+                                        viewModel.fetchNFTsIfNeeded(for: nft)
+                                    }
+                                    .onTapGesture {
+                                        vibrate(.heavy)
+                                        viewModel.handleTapOn(nft: nft)
+                                    }
+                            }
+                        }
+                        .padding(EdgeInsets(top: 30, leading: 10, bottom: 30, trailing: 10))
+                    }
+                    .navigationTitle("My NFTs")
                 }
-                .frame(height: 15)
-                .padding()
-                ScrollView {
-                    LazyVStack(spacing: 40) {
-                        ForEach(viewModel.nftsViewModel, id: \.id) { nft in
-                            NFTView(nft: nft)
-                                .equatable()
-                                .cardStyle()
-                                .onAppear {
-                                    viewModel.fetchNFTsIfNeeded(for: nft)
+                BottomCardView(
+                    show: $viewModel.showDetails,
+                    model: $viewModel.nftDetails
+                ) { nft in
+                    ScrollView {
+                        VStack(alignment: .leading, spacing: 10) {
+                            HStack {
+                                PillView(text: "#\(nft.tokenId)")
+                                Spacer()
+                                VStack {
+                                    Image(uiImage: Images.share)
                                 }
+                                .frame(width: 40, height: 40)
                                 .onTapGesture {
-                                    vibrate(.heavy)
-                                    viewModel.handleTapOn(nft: nft)
+                                    viewModel.share(nft: nft)
                                 }
-                        }
-                    }
-                    .padding(EdgeInsets(top: 30, leading: 10, bottom: 30, trailing: 10))
-                }
-            }
-            BottomCardView(
-                show: $viewModel.showDetails,
-                model: $viewModel.nftDetails
-            ) { nft in
-                ScrollView {
-                    VStack(alignment: .leading, spacing: 10) {
-                        HStack {
-                            PillView(text: "#\(nft.tokenId)")
-                            Spacer()
-                            VStack {
-                                Image(uiImage: Images.share)
                             }
-                            .frame(width: 40, height: 40)
-                            .onTapGesture {
-                                viewModel.share(nft: nft)
+                            Title(nft.name)
+                            Text(nft.description ?? nft.name)
+                                .multilineTextAlignment(.leading)
+                            Text("Attributes")
+                                .font(.title2)
+                                .bold()
+                                .multilineTextAlignment(.leading)
+                            MultilineHStack(models: nft.attributes) { attribute in
+                                AttributeView(
+                                    trait: attribute.trait,
+                                    value: attribute.value.isEmpty ? "n/a" : attribute.value
+                                )
                             }
                         }
-                        Title(nft.name)
-                        Text(nft.description ?? nft.name)
-                            .multilineTextAlignment(.leading)
-                        Text("Attributes")
-                            .font(.title2)
-                            .bold()
-                            .multilineTextAlignment(.leading)
-                        // TODO Attribute view looks messed up when no value or when value is long/multi-line
-                        MultilineHStack(models: nft.attributes) { attribute in
-                            AttributeView(
-                                trait: attribute.trait,
-                                value: attribute.value.isEmpty ? "n/a" : attribute.value
-                            )
-                        }
+                        .frame(
+                            minWidth: 0,
+                            maxWidth: .infinity,
+                            minHeight: 0,
+                            maxHeight: .infinity,
+                            alignment: .topLeading
+                        )
                     }
-                    .frame(
-                        minWidth: 0,
-                        maxWidth: .infinity,
-                        minHeight: 0,
-                        maxHeight: .infinity,
-                        alignment: .topLeading
-                    )
                 }
-            }
-        }.sheet(isPresented: $viewModel.showShareMedia) {
-            if let urlString = viewModel.sharedMedia?.url,
-               let url = URL(string: urlString),
-               let image = viewModel.sharedMedia?.fileType == .svg
-                    ? svgImage(contentsOfFile: url.path)
-                    : UIImage(contentsOfFile: url.path) {
-                ShareSheet(activityItems: ["", image]) { _,_,_,_ in
-                    viewModel.showDetails = false
+            }.sheet(isPresented: $viewModel.showShareMedia) {
+                if let urlString = viewModel.sharedMedia?.url,
+                   let url = URL(string: urlString),
+                   let image = viewModel.sharedMedia?.fileType == .svg
+                        ? svgImage(contentsOfFile: url.path)
+                        : UIImage(contentsOfFile: url.path) {
+                    ShareSheet(activityItems: ["", image]) { _,_,_,_ in
+                        viewModel.showDetails = false
+                    }
                 }
             }
         }
